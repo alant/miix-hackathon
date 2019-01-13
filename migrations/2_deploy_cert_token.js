da9e6aaa6a632b130643faed3947f1f98ebb00d1
Fan, Linlin <fanll@forkcloud.com>
2019-01-13 11:07:58 +0800

    fix mig

--- migrations/2_deploy_cert_token.js
+++ migrations/2_deploy_cert_token.js
@@ -2,9 +2,5 @@ var CertToken = artifacts.require("./CertToken.sol");
 var Registration = artifacts.require("./Registration.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(CertToken);
-  deployer.deploy(CertToken).then(
-    function(){
-      deployer.deploy(Registration(CertToken.address));
-    });
+  deployer.deploy(CertToken);
 };
